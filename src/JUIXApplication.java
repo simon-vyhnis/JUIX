7ea854f924c95641c689b2029c6b8bb5cc12dbf4
Šimon Vyhnis <simon.vyhnis@gmail.com>
2020-11-15 21:20:43 +0100

    JUIX application basic functionality

--- src/JUIXApplication.java
+++ src/JUIXApplication.java
@@ -3,6 +3,12 @@ import java.awt.*;
 
 public class JUIXApplication {
     private JFrame frame;
+    private Bounds bounds;
+    private Part currentPart;
+
+    /*
+     * If you do not need anything else to to define than name and start Part than use this constructor.
+     */
     public JUIXApplication(String name, Part startPart){
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         frame = new JFrame();
@@ -10,10 +16,24 @@ public class JUIXApplication {
         frame.setBounds(0, 0, screenSize.width, screenSize.height);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
+
+    /*
+     * Use this constructor if you want to build JUIX application with config file.
+     * A config file have to contain at least name and start part. Learn more about config files <a>here<a/>.
+     */
     public JUIXApplication(JUIXFile configFile){
-
+        //TODO: create this
     }
-    public void start(){
 
+    public void start(){
+        frame.setVisible(true);
+    }
+
+    /*
+     * Returns the JFrame, that the app is displayed on.
+     * You should not access the frame manually, but if you are sure, that there isn't other way, than use this method.
+     */
+    public JFrame getFrame(){
+        return frame;
     }
 }
---
+++ src/TestingClass.java
@@ -0,0 +1,7 @@
+public class TestingClass {
+
+    public static void main(String[] args){
+        JUIXApplication application = new JUIXApplication("Testing App", new Part() {});
+        application.start();
+    }
+}
